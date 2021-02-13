5af9294a898d82a9e1d706ed75b49210683105f6
aks1809 <akshay.chaturvedi.1997@gmail.com>
2021-02-13 12:01:32 +0530

    public keys redressal

--- config/keys.js
+++
@@ -1,9 +0,0 @@
-const keys = {
-  clientID:
-    "629943494069-pvpoltmqegru09erc7j504e3tj20enh0.apps.googleusercontent.com",
-  clientSecret: "A6Ik98AO7NcpQeoz9spuSQjc",
-  mongo_uri:
-    "mongodb+srv://akshay:akshay@cluster0.abjmv.mongodb.net/blogs?retryWrites=true&w=majority",
-};
-
-export default keys;
